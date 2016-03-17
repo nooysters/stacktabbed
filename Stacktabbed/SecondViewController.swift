0629d4f8f0234b9ca0659082b5e5d5afccbb07b1
Jeremy <jeremy@stacksocial.com>
2016-03-17 13:52:56 -0700

    Initial Commit

---
+++ Stacktabbed/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Stacktabbed
+//
+//  Created by Jeremy Azzari on 3/17/16.
+//  Copyright © 2016 Jeremy Azzari. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
